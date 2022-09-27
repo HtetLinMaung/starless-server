72c5b4f584d6fd3f94f56a07f329dfa21fb23d82
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2022-09-27 17:43:30 +0630

    update

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ import path from "path";
 import chalk from "chalk";
 import fs from "fs";
 import { isAsyncFunction } from "util/types";
-import multer from "multer";
 
 import swaggerUi from "swagger-ui-express";
 import getFiles from "./utils/get-files";
